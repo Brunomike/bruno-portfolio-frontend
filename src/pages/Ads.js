c06826819a36f95f322ef83dbf91a5625bc517bc
Michael Bruno <brunomike254@gmail.com>
2022-08-27 00:28:50 +0300

    bug fix

--- src/pages/Ads.js
+++ src/pages/Ads.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 const Ads = () => {
+useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://www.star-clicks.com/secure/ads.php?pid=69811193650677154';
+    script.async = true;
+
+    document.getElementsByClassName('ad__bruno').appendChild(script);
+    //<script type='text/javascript' src='https://www.star-clicks.com/secure/ads.php?pid=69811193650677154'></script>
+
+}, [])
+
+
     return (
-        <div>            
-            <script type='text/javascript' src='https://www.star-clicks.com/secure/ads.php?pid=69811193650677154'></script>            
+        <div className='ad__bruno'>            
+            
         </div>
     )
 }
